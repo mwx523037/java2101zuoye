256dd903f354eea3f0a504fef8bbc98667b67826
mwx523037 <2241365986@qq.com>
2021-06-19 16:45:00 +0800

    山东福瑞达

--- Test.java
+++ Test.java
@@ -4,11 +4,11 @@ import java.util.Arrays;
 
 public class Test {
     public static void main(String[] args) {
-        Student student = new Student("徐伟",29, Arrays.asList("唱歌","跳舞"));
+        Student student = new Student("张静",29, Arrays.asList("唱歌","跳舞"));
         String stuJson=JSON.toJSON(student).toString();
         System.out.println(stuJson);
 
-        //json字符串转化为对象
+        //json字符串转化为对象宋厚宇
         String stuJson2 = "{\"skills\":[\"唱歌\",\"跳舞\"],\"name\":\"徐伟\",\"age\":29}";
         Student student1 = JSON.parseObject(stuJson2,Student.class);
         System.out.println("转化为学生对象："+student1.getName());
